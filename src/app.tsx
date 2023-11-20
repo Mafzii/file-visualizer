05aacaf9a7611ac2abf79bfdd90402383ac0aed5
Mafzii <23100040@lums.edu.pk>
2023-11-21 00:01:17 +0500

    Config: Fixed react-dom setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./components/Home";
 
-const root = createRoot(document.body);
-root.render(
-    <div>
-        <h1>File Visualizer</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-    );
+const root = createRoot(document.getElementById("root"));
+root.render(<Home />);
