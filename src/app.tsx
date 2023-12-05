ae49a32bb2c9fba427ef8e27ef97049a6122ad4d
Mafzii <23100040@lums.edu.pk>
2023-12-05 23:50:14 +0500

    Config: Added material ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Home from "./components/Home";
+import Home from "./components/Dashboard";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<Home />);
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Dashboard() {
+    return ( 
+        <div>
+            <h1 className="text-3xl font-bold">Dashboard</h1>
+            <Navbar />
+        </div>
+    );
+}
+
+export default Dashboard;
