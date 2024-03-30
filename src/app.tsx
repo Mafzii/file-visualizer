999f23988d9856045a84aa968b1843e4c7bcb672
Mafzii <23100040@lums.edu.pk>
2024-03-30 14:29:37 +0500

    Config: Restructured application

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Home from "./components/Dashboard";
+import Home from "./components/home/Home";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<Home />);
