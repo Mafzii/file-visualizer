c1b371cb3c7d7f5d85a07ee58292df5ce8ffe551
Mafzii <23100040@lums.edu.pk>
2024-04-15 22:52:42 +0500

    Navbar: Started work and styling of overall app

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Graph from "../graph/Graph";
+import Navbar from "../navbar/Navbar";
 
 function Home() {
   return (
     <div className="bg-base-100 w-full h-full">
-      <h1 className="text-3xl font-bold">fileWiz</h1>
-      <Graph />
+      <h1 className="text-3xl font-bold">fileWiz 🪄</h1>
+      <main className="flex">
+        <section id="navbar" className="">
+          <Navbar />
+        </section>
+        <section id="graph" className="">
+          <Graph />
+        </section>
+      </main>
     </div>
   );
 }
